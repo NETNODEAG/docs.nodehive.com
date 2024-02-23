8e826f76ba3d97e248bcaff40d7865d4d94f39f4
lukas.fischer <lukas.fischer@netnode.ch>
2024-02-23 10:00:43 +0100

    added content

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,27 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
+					label: 'Developer Docs',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Introduction', link: '/overview/introduction/' },
+						{ label: 'Quickstart', link: '/overview/quickstart/' },
+						{ label: 'NodeHive SaaS', link: '/overview/nodehive-saas/' },
+						{ label: 'FAQ', link: '/overview/faq/' },
+						{ label: 'Videos', link: '/overview/videos/' },
+						{ label: 'About', link: '/overview/about/' },
+					],
+				},
+				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
+						{ label: 'Content Modelling', link: '/guides/content-modelling/' },
+						{ label: 'API Reference', link: '/guides/intro-api/' },
+						{ label: 'Frontend starter', link: '/guides/build-frontend/' },
+						{ label: 'Selfhost NodeHive', link: '/guides/selfhost-nodehive/' },
+						{ label: 'Multilingual', link: '/guides/multilingual/' },
+						{ label: 'Visual Editor', link: '/guides/visual-editor/' },
 						{ label: 'Example Guide', link: '/guides/example/' },
 					],
 				},
